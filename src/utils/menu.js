d7fc08b73d198862dbdf8bb87f3ed1ae95586a25
pmg1989 <972401854@qq.com>
2017-03-11 14:26:13 +0800

    移除多余的order/list

--- src/utils/menu.js
+++ src/utils/menu.js
@@ -68,116 +68,12 @@ const menu = [
     children: [
       {
         id: _.uniqueId(),
-        key: 'list',
+        key: 'flow',
         name: '订单列表',
         power: [1, 2]
-      },
-      {
-        id: _.uniqueId(),
-        key: 'flow',
-        name: '账户流水',
-        power: [1, 2]
       }
     ]
-  },
-  //live
-  {
-    id: _.uniqueId(),
-    key: 'live',
-    name: '直播管理',
-    icon: 'play-circle-o',
-    clickable: false,
-    power: [1],
-    children: [
-      {
-        id: _.uniqueId(),
-        key: 'list',
-        name: '直播列表',
-        power: [1, 2]
-      }
-    ]
-  },
-  //bbs
-  {
-    id: _.uniqueId(),
-    key: 'bbs',
-    name: '论坛管理',
-    icon: 'message',
-    clickable: false,
-    power: [1],
-    children: [
-      {
-        id: _.uniqueId(),
-        key: 'message',
-        name: '帖子列表',
-        power: [1, 2]
-      },
-      {
-        id: _.uniqueId(),
-        key: 'reply',
-        name: '回复列表',
-        power: [1, 2]
-      },
-      {
-        id: _.uniqueId(),
-        key: 'recommend',
-        name: '小编推荐',
-        power: [1, 2]
-      },
-      {
-        id: _.uniqueId(),
-        key: 'category',
-        name: '分类管理',
-        power: [1, 2, 3, 4, 5]
-      },
-      {
-        id: _.uniqueId(),
-        key: 'label',
-        name: '标签管理',
-        power: [1, 2]
-      },
-      {
-        id: _.uniqueId(),
-        key: 'silence',
-        name: '禁言管理',
-        power: [1, 2]
-      }
-    ]
-  },
-  //score
-  {
-    id: _.uniqueId(),
-    key: 'score',
-    name: '积分管理',
-    icon: 'pay-circle-o',
-    clickable: false,
-    power: [1],
-    children: [
-      {
-        id: _.uniqueId(),
-        key: 'change',
-        name: '积分变更',
-        power: [1, 2]
-      }
-    ]
-  },
-  //course
-  {
-    id: _.uniqueId(),
-    key: 'course',
-    name: '课程管理',
-    icon: 'book',
-    clickable: false,
-    power: [1],
-    children: [
-      {
-        id: _.uniqueId(),
-        key: 'list',
-        name: '课程列表',
-        power: [1, 2]
-      }
-    ]
-  },
+  }
 ]
 
 export default menu
