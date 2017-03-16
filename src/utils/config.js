16b87eb0c6f266fbd6ad4d83f6df4215969fd5cd
pmg1989 <972401854@qq.com>
2017-03-16 17:03:17 +0800

    fix config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,9 +1,9 @@
 
 export default  {
-  name: 'NewBand App Administrator',
-  prefix: '',//'NBAdmin',
-  footerText: 'NewBand 版权所有 © 2016 由 felixpmg 提供支持',
+  name: 'dva-admin',
+  prefix: '',
+  footerText: 'dva-admin 版权所有 © 2016 由 pmg1989 提供支持',
   logoSrc: 'https://o9u2lnvze.qnssl.com/web/global/brand.png',
-  logoText: 'NewBand Admin',
+  logoText: 'DVA Admin',
   needLogin: true
 }
