7c6faab7d5ee5e72a7d2feeed30ca3782a46b0c9
pmg1989 <972401854@qq.com>
2017-03-22 18:11:21 +0800

    fix ui/drop-menu.js

--- src/routes/ui/DropMenu.js
+++ src/routes/ui/DropMenu.js
@@ -1,71 +1,64 @@
 import React from 'react'
 import { connect } from 'dva'
 import { Link } from 'dva/router'
-import { Menu, Icon, Row, Col, Card, message } from 'antd'
+import { Menu, Icon, Row, Col, Card, message, Modal } from 'antd'
 import DropMenu from '../../components/common/DropMenu'
-import { DETAIL, UPDATE, DELETE, STATUS, ADD } from '../../constants/options'
+import { DETAIL, UPDATE, DELETE } from '../../constants/options'
 
-const updatePower = true
-const deletePower = true
-const detailPower = true
-const statusPower = true
+const confirm = Modal.confirm
 
 const handleClick = (key) => {
   message.success(`你点击了Key：${key}`)
 }
 
-const dropMenuProps = {
-  updatePower,
-  detailPower,
-  deletePower,
-  onMenuClick: ({ key }) => {
-    return {
-      [DETAIL]: handleClick,
-      [UPDATE]: handleClick,
-      [DELETE]: handleClick,
-      [STATUS]: handleClick,
-      [ADD]:    handleClick
-    } [key] (key)
-  }
+const handleDeleteItem = (key) => {
+  confirm({
+    title: '您确定要删除这条记录吗?',
+    onOk() {
+      handleClick(key)
+    }
+  })
+}
+
+const handleMenuClick = ({ key }) => {
+  return {
+    [DETAIL]: handleClick,
+    [UPDATE]: handleClick,
+    [DELETE]: handleDeleteItem
+  } [key] (key)
 }
 
 const DropMenuPage = () => (
   <div className="content-inner">
-    <h2 style={{ margin: '16px 0' }}>带权限验证的DropMenu使用示例</h2>
     <Row gutter={32}>
       <Col lg={8} md={12}>
         <Card title="基础菜单">
-          <DropMenu {...dropMenuProps} />
-        </Card>
-      </Col>
-      <Col lg={8} md={12}>
-        <Card title="边框式基础菜单">
-          <DropMenu updatePower deletePower border dropDownProps={{placement: 'bottomLeft'}}  onMenuClick={dropMenuProps.onMenuClick}/>
-        </Card>
-      </Col>
-      <Col lg={8} md={12}>
-        <Card title="拓展菜单功能">
-          <DropMenu {...dropMenuProps}>
-            {statusPower && <Menu.Item key={STATUS}>禁用</Menu.Item>}
-          </DropMenu>
-        </Card>
-      </Col>
-    </Row>
-    <Row gutter={32}>
-      <Col lg={8} md={12}>
-        <Card title="拓展菜单覆盖基础菜单">
-          <DropMenu detailPower deletePower onMenuClick={dropMenuProps.onMenuClick}>
-            {statusPower && <Menu.Item key={STATUS}>禁用</Menu.Item>}
-            {updatePower && <Menu.Item key={UPDATE}><Link to="/">转页编辑</Link></Menu.Item>}
+          <DropMenu>
+            <Menu onClick={handleMenuClick}>
+              <Menu.Item key={UPDATE}>编辑</Menu.Item>
+              <Menu.Item key={DELETE}>删除</Menu.Item>
+            </Menu>
           </DropMenu>
         </Card>
       </Col>
       <Col lg={8} md={12}>
-        <Card title="完全自定义菜单">
-          <DropMenu onMenuClick={dropMenuProps.onMenuClick}>
-            <Menu.Item key={ADD}>新增</Menu.Item>
-            <Menu.Item key={STATUS}>审核</Menu.Item>
-            <Menu.Item key={UPDATE}><Link to="/">转页编辑</Link></Menu.Item>
+        <Card title="边框式菜单">
+          <DropMenu border>
+            <Menu onClick={handleMenuClick}>
+              <Menu.Item key={UPDATE}>编辑</Menu.Item>
+              <Menu.Item key={DELETE}>删除</Menu.Item>
+            </Menu>
+          </DropMenu>
+        </Card>
+      </Col>
+      <Col lg={8} md={12}>
+        <Card title="点击式菜单">
+          <DropMenu dropDownProps={{trigger: ['click']}}>
+            <Menu onClick={handleMenuClick}>
+              <Menu.Item key={DETAIL}>详情</Menu.Item>
+              <Menu.Item key={UPDATE}>编辑</Menu.Item>
+              <Menu.Item key={DELETE}>删除</Menu.Item>
+            </Menu>
           </DropMenu>
         </Card>
       </Col>
