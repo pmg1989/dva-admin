6dde214bf5ae087ae1cfafc35a69316b82ec0e8b
pmg1989 <972401854@qq.com>
2017-03-29 15:07:49 +0800

    update theme.js

--- src/theme.js
+++ src/theme.js
@@ -7,7 +7,7 @@ module.exports = () => {
     '@border-color-split': '#f4f4f4',
     '@border-color-base': '#e5e5e5',
     '@menu-dark-bg': '#3e3e3e',
-    '@text-color': '#666',
-    '@font-family': 'monospace,-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", SimSun, sans-serif'
+    '@text-color': '#333',
+    '@font-family': '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "Helvetica Neue", Helvetica, Arial, sans-serif'
   }
 }
