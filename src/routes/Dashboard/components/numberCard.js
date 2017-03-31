27160949f0609eb2403ff8342ecaa681683c6b38
felixpmg <felix.pan@newband.com>
2017-03-31 10:19:48 +0800

    numberCard styles

--- src/routes/Dashboard/components/numberCard.js
+++ src/routes/Dashboard/components/numberCard.js
@@ -7,20 +7,22 @@ function NumberCard (props) {
   const { icon, color, title, number } = props
   return (
     <Card className={styles.numberCard} bordered={false} bodyStyle={{padding: 0}}>
-      <Icon className={styles.iconWarp} style={{ color }} type={icon} />
-      <div className={styles.content}>
-        <p className={styles.title}>{title || 'No Title'}</p>
-        <p className={styles.number}>
-          <CountUp
-            start={0}
-            end={number}
-            duration={2.75}
-            useEasing
-            useGrouping
-            separator=','
-            {...props.countUp || {}}
-          />
-        </p>
+      <div className={styles.flexBox}>
+        <Icon className={styles.iconWarp} style={{ backgroundColor : color }} type={icon} />
+        <div className={styles.content}>
+          <p className={styles.title}>{title || 'No Title'}</p>
+          <p className={styles.number}>
+            <CountUp
+              start={0}
+              end={number}
+              duration={2.75}
+              useEasing
+              useGrouping
+              separator=','
+              {...props.countUp || {}}
+            />
+          </p>
+        </div>
       </div>
     </Card>
   )
