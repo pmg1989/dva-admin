f58ed69c12725b5e06a6557420233ce56ecc4ac8
pmg1989 <972401854@qq.com>
2017-03-31 15:02:08 +0800

    fix numberCard bugs

--- src/routes/Demo/index.js
+++ src/routes/Demo/index.js
@@ -1,11 +1,60 @@
 import React, {Component} from 'react'
 import {connect} from 'dva'
+import { Tabs } from 'antd'
 
-class Demo extends Component {
+const TabPane = Tabs.TabPane
+
+class Demo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.newTabIndex = 0
+    const panes = [
+      { title: 'Tab 1', content: 'Content of Tab 1', key: '1', closable: false },
+      { title: 'Tab 2', content: 'Content of Tab 2', key: '2' },
+    ]
+    this.state = {
+      activeKey: panes[0].key,
+      panes
+    }
+  }
+
+  onChange = (activeKey) => {
+    this.setState({ activeKey })
+  }
+  onEdit = (targetKey, action) => {
+    this[action](targetKey)
+  }
+  add = () => {
+    const panes = this.state.panes
+    const activeKey = `newTab${this.newTabIndex++}`
+    panes.push({ title: 'New Tab', content: 'Content of new Tab', key: activeKey })
+    this.setState({ panes, activeKey })
+  }
+  remove = (targetKey) => {
+    let activeKey = this.state.activeKey;
+    let lastIndex
+    this.state.panes.forEach((pane, i) => {
+      if (pane.key === targetKey) {
+        lastIndex = i - 1
+      }
+    });
+    const panes = this.state.panes.filter(pane => pane.key !== targetKey)
+    if (lastIndex >= 0 && activeKey === targetKey) {
+      activeKey = panes[lastIndex].key
+    }
+    this.setState({ panes, activeKey })
+  }
   render() {
     return (
-      <div>Demo</div>
-    )
+      <Tabs
+        onChange={this.onChange}
+        activeKey={this.state.activeKey}
+        type="editable-card"
+        onEdit={this.onEdit}
+      >
+        {this.state.panes.map(pane => <TabPane tab={pane.title} key={pane.key} closable={pane.closable}>{pane.content}</TabPane>)}
+      </Tabs>
+    );
   }
 }
 
