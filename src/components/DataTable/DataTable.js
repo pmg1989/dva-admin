ce030a224a8e48a1f2e5ed90af167d857dbbedac
pmg1989 <972401854@qq.com>
2017-04-14 11:42:41 +0800

    fix DataTable.js

--- src/components/DataTable/DataTable.js
+++ src/components/DataTable/DataTable.js
@@ -5,7 +5,7 @@ import {Table} from 'antd'
 import classnames from 'classnames'
 import TableBodyWrapper from './TableBodyWrapper'
 
-function DataTable({dispatch, location, curTab, className, pagination, animate, ...props}) {
+function DataTable({dispatch, location, className, pagination, animate, ...props}) {
 
   const getBodyWrapperProps = {
     page: location.query.page || 1,
@@ -16,7 +16,7 @@ function DataTable({dispatch, location, curTab, className, pagination, animate, 
 
   const onPageChange = (page) => {
     const {query} = location
-    const pathname = curTab.path || location.pathname
+    const pathname = location.pathname
     dispatch(routerRedux.push({
       pathname: pathname,
       query: {
@@ -65,7 +65,7 @@ DataTable.defaultProps = {
 }
 
 function mapStateToProps({ routing, app }) {
-  return { location: routing.locationBeforeTransitions, curTab: app.curTab }
+  return { location: routing.locationBeforeTransitions }
 }
 
 export default connect(mapStateToProps)(DataTable)
