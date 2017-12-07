250f5784e9e071aa39903006da92ea5ec90175f3
pmg1989 <972401854@qq.com>
2017-12-07 10:41:13 +0800

    fix DataTable.js

--- src/components/DataTable/DataTable.js
+++ src/components/DataTable/DataTable.js
@@ -1,65 +1,68 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'dva'
 import { Table } from 'antd'
 import classnames from 'classnames'
 import TableBodyWrapper from './TableBodyWrapper'
 
-function DataTable ({ dispatch, location, className, pagination, onPageChange, animate, ...props }) {
-  const getBodyWrapperProps = {
-    page: location.query.current || 1,
-    current: pagination.current || 1,
+class DataTable extends Component {
+  static propTypes = {
+    dispatch: PropTypes.func.isRequired,
+    location: PropTypes.object,
+    animate: PropTypes.bool,
+    rowKey: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.func,
+    ]).isRequired,
+    pagination: PropTypes.oneOfType([
+      PropTypes.bool,
+      PropTypes.object,
+    ]).isRequired,
+    columns: PropTypes.array.isRequired,
+    dataSource: PropTypes.array.isRequired,
+    className: PropTypes.string,
+    onPageChange: PropTypes.func,
   }
 
-  const getBodyWrapper = body => (<TableBodyWrapper {...getBodyWrapperProps} body={body} />)
+  static defaultProps = {
+    animate: true,
+  }
 
-  const handlePageChange = (page) => {
-    const { query } = location
-    onPageChange({
+  handlePageChange = (page) => {
+    const { query } = this.props.location
+    this.props.onPageChange({
       ...query,
       current: page.current,
       pageSize: page.pageSize,
     })
   }
 
-  let tableProps = {
-    simple: true,
-    bordered: true,
-    scroll: { x: 1200 },
-    onChange: handlePageChange,
-    pagination: !!pagination && { ...pagination, showSizeChanger: true, showQuickJumper: true, showTotal: total => `共 ${total} 条` },
-    ...props,
+  render () {
+    const { dispatch, location, className, pagination, onPageChange, animate, ...props } = this.props
+    const getBodyWrapperProps = {
+      page: location.query.current || 1,
+      current: pagination.current || 1,
+    }
+
+    const getBodyWrapper = body => (<TableBodyWrapper {...getBodyWrapperProps} body={body} />)
+
+    let tableProps = {
+      simple: true,
+      bordered: true,
+      scroll: { x: 1200 },
+      onChange: this.handlePageChange,
+      pagination: !!pagination && { ...pagination, showSizeChanger: true, showQuickJumper: true, showTotal: total => `共 ${total} 条` },
+      ...props,
+    }
+    if (animate) {
+      tableProps.getBodyWrapper = getBodyWrapper
+      tableProps.className = classnames(className, 'table-motion')
+    }
+
+    return (
+      <Table {...tableProps} />
+    )
   }
-  if (animate) {
-    tableProps.getBodyWrapper = getBodyWrapper
-    tableProps.className = classnames(className, 'table-motion')
-  }
-
-  return (
-    <Table {...tableProps} />
-  )
-}
-
-DataTable.propTypes = {
-  dispatch: PropTypes.func,
-  location: PropTypes.object,
-  animate: PropTypes.bool,
-  rowKey: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.func,
-  ]).isRequired,
-  pagination: PropTypes.oneOfType([
-    PropTypes.bool,
-    PropTypes.object,
-  ]).isRequired,
-  columns: PropTypes.array.isRequired,
-  dataSource: PropTypes.array.isRequired,
-  className: PropTypes.string,
-  onPageChange: PropTypes.func,
-}
-
-DataTable.defaultProps = {
-  animate: true,
 }
 
 function mapStateToProps ({ routing }) {
